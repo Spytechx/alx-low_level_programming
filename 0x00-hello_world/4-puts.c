de29172143434b9d4456b5bbc26fe9e9073a7243
spytechx <samuraidovexx@gmail.com>
2022-12-08 16:27:35 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Prints some lines of code without using printf but using put
+ *
+ * Return: Always print 0 as answer
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle);
+	return (0);
+}
