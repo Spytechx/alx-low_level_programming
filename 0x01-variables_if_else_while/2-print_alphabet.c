24af202f489e5ef6cd12ff91a39a66c1438c183f
spytechx <samuraidovexx@gmail.com>
2022-12-09 16:30:56 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for functio main goes there */
+/**
+ * main - this is the beginning point of the program
+ *
+ * Return: default value 0 returns
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+
+	putchar(10);
+
+	return (0);
+}
