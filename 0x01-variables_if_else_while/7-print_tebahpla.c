0417a0c304c7156afd9d7e8139724df5c03d87c9
spytechx <samuraidovexx@gmail.com>
2022-12-09 18:53:16 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- start point of the program
+ *
+ * Return: returns a default value of 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+
+	putchar(10);
+	return (0);
+}
