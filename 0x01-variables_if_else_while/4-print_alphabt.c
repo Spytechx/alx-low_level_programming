e5a145e590c49eab149cdf30a8730ec413413478
spytechx <samuraidovexx@gmail.com>
2022-12-09 18:03:20 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main- this is the beginning of the program
+ *
+ * Return: default value of 0 returns
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+		if (i != 101 && i != 113)
+			putchar(i);
+	
+	putchar(10);
+	
+	return (0);
+
+}
