bcf7526c552fc271c71c4062b0c6d5a80bd1c9cc
spytechx <samuraidovexx@gmail.com>
2022-12-09 18:19:25 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main- this is the beginning of the program
+*
+* Return:default value of 0 returns
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+}
