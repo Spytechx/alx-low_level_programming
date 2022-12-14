ac8c05b096877aa3a71df549014a7e13ed8825ba
spytechx <samuraidovexx@gmail.com>
2022-12-13 18:40:05 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabets in lower case
+ *
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x ='a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
