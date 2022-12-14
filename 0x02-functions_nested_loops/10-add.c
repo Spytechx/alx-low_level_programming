ccfced55bb98b9fdb6001db0ad268a3effd3b377
spytechx <samuraidovexx@gmail.com>
2022-12-13 21:00:05 -0800

    JH

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - STart Point
+ * @a: first input
+ * @b: second input
+ * Description: this function returns 1 or 0
+ * Return: 1 (success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
