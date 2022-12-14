e5252359ac835f2dd0b618c1600843aa602413d1
spytechx <samuraidovexx@gmail.com>
2022-12-13 20:23:11 -0800

    6-main.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Start point
+ * @x: takes input
+ * Description: FUnction returns 0 or 1
+ * Return: 0 is false and 1 is true
+ */
+
+int _abs(int x)
+{
+	if (n < 0)
+	{
+		return (x * -1);
+	}
+	return (n);
+}
