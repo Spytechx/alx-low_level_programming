193fa6c8c38db9a1a3347d4fecc7ff193a7b9fbc
spytechx <samuraidovexx@gmail.com>
2022-12-15 15:47:53 -0800

    nsssf

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply integers
+ * @a: first intake
+ * @b: second intake
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ * _putchar - print
+ */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
