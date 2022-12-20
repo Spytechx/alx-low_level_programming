af4800f0b1699c73883b9af1091891085826eb82
spytechx <samuraidovexx@gmail.com>
2022-12-19 16:37:52 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * _putchar - print each character
+ * @s: char for input
+ * Return: null
+ */
+
+void rev_string(char *s)
+{
+	int x = 0, y, z;
+	char m;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	z = x - 1;
+	for (y = 0; z >= 0 && y < z; c--, y++)
+	{
+		m = s[y];
+		s[y] = s[z];
+		s[z] = m;
+	}
+}
