a24374a839ab9c790f13c919955530cebf0d13de
spytechx <samuraidovexx@gmail.com>
2022-12-19 16:23:59 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev- prints a reverse string
+ * @s: string thats printed
+ * Return: none
+ */
+
+void print_rev(char *s)
+{
+	int x, y;
+
+	y = 0;
+
+	while (s[y] != '\0')
+		y++;
+	for (x = y - 1; x >= 0; x--)
+	{
+		_putchar([x]);
+	}
+	_putchar('\n');
+}
