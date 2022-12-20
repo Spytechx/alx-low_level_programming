e87d27c64594ac92c7545dac9e0b100366128b53
spytechx <samuraidovexx@gmail.com>
2022-12-19 16:43:46 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - pritns all other characters of string
+ * @str: receiver charcter to watch
+ * Return: null
+ */
+
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+		if (string % 2 == 0)
+			_putchar(str[string]);
+	_putchar('\n');
+}
