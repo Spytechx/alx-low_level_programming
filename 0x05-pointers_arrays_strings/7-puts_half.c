7f3cb60f21b32917f1117d2b18c9ba0608d909f9
spytechx <samuraidovexx@gmail.com>
2022-12-19 17:24:58 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,17 +8,17 @@
 
 void puts_half(char *str)
 {
-	int rec = 0, x;
+	int string = 0, x;
 
-	while (str[rec] != '\0')
-		rec++;
-	if (rec + 1 % 2 != '0')
-		x = (rec - 1) / 2;
+	while (str[string] != '\0')
+		string++;
+	if (string + 1 % 2 != '0')
+		x = (string - 1) / 2;
 	else
-		x = (rec / 2);
+		x = (string / 2);
 	x++;
 
-	for (rec = x; str[rec] != '\0'; rec++)
-		_putchar(str[rec]);
+	for (string = x; str[string] != '\0'; string++)
+		_putchar(str[string]);
 	_putchar('\0');
 }
