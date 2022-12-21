b95b4e5f43912591eced8efd0e56301d7b25b9e6
spytechx <samuraidovexx@gmail.com>
2022-12-21 08:32:52 -0800

    Task 0x06

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _strcat - concat 2 string
+/**
+ * _strcat - concat 2 string
  * @dest:char
  * @src:char
  * Return:char
@@ -9,6 +10,7 @@
 char *_strcat(char *dest, char *src)
 {
 	char *s = dest;
+
 	while (*dest != '\0')
 	{
 		dest++;
