34b16b226d6fb268fb295ad81d878a1557455c8a
spytechx <samuraidovexx@gmail.com>
2023-01-03 15:05:37 -0800

    TASK 0x07

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -6,16 +6,16 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     char *s = "hello";
     char *f;
-
     f = _strchr(s, 'l');
-
+    
     if (f != NULL)
     {
-        printf("%s\n", f);
+	    printf("%s\n", f);
     }
     return (0);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <string.h> 
 
 /**
  * _strchr - locates a character in a string
  * @c: returns pointer to first occurrence of the character
  * @s: in string
+ * NULL: shdj
  * Return: A pointer to the first occurrence of character
  */
 
