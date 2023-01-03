dab7783dddee7c688e8e2787c8bced8b0c8a3af1
Spytechx <samuraidovexx@gmail.com>
2023-01-03 12:23:59 +0100

    TASK 0x07

---
+++ 0x07-pointers_arrays_strings/8-print_disgsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sums of the two diagonals of square matrix
+ * @a: pointer to start of matrix
+ * @size: width of matrix column
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int x, y, p, m = 0, n = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		p = (x * size) + x;
+		m += *(a + p);
+	}
+	for (y = 0; y < size; y++)
+	{
+		p = (y * size) + (size - 1 - y);
+		n += *(a + p);
+	}
+	printf("%i, %i\n", m, n);
+}
