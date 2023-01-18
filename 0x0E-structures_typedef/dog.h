f8bbb0bbba039bb6780910bd318ed42403ebbd82
spytechx <samuraidovexx@gmail.com>
2023-01-17 18:10:30 -0800

    0x0E. C - Structures, typedef Tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,19 @@
 #define DOG
 
 /**
- *  * struct dog - dog class
- *   *
- *    * @name: Name of the dog
- *     *
- *      * @age: Age of the dog
- *       *
- *        * @owner: Owner of the dog
- *         */
+ * struct dog - dog class
+ * @name: Name of the dog
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ *
+ */
 
 struct dog
 {
-	        char *name;
-		        float age;
-			        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
