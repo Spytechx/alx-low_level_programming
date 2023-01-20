4ca00b4531b8de14923b63b04227216b31113824
spytechx <samuraidovexx@gmail.com>
2023-01-19 23:19:31 -0800

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,17 +10,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
 	unsigned int i;
-	va_list ap;
+	va_list m;
 
-	va_start(ap, n)
+	va_start(m, n);
 
 		if (n == 0)
 			return (0);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(m, int);
 
-	va_end(ap);
+	va_end(m);
 
 	return (sum);
 }
