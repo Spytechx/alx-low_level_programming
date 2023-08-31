b4117f822736a46034dce7b49e7ad467a12f8d47
SPytechx <samuraidovexx@gmail.com>
2023-08-31 16:14:38 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *b);
+unsigned long int_power(unsigned int base, unsigned int pow);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+unsigned clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _putchar(char c);
+
+#endif
+
+
